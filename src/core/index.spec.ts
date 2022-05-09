e176bdcb1e2d5f74720e3f2c895cf449bad52aea
Julien <jujufrancois1999@gmail.com>
2022-05-09 14:45:47 +0200

    Add features Dashboard option

--- src/core/index.spec.ts
+++ src/core/index.spec.ts
@@ -1,12 +1,13 @@
 // @ts-ignore see https://github.com/jest-community/jest-extended#setup
 import * as matchers from 'jest-extended';
 expect.extend(matchers);
+import { Cell } from '../core';
 
-describe('Test the creation of the first cells', function () {
-  // it('Should pass', function () {
-  //   const dimensions: [number, number] = [1000, 1000];
-  //   // Check if rendom cell coords are outside the grid.
-  //   expect(dimensions.length).toEqual(100);
-  //   // Create a loop that verifies the randomly generated cells coordinants
-  // });
+describe('Test constructor of class Cell.', function () {
+  it('Should pass', function () {
+    const newCell = new Cell(1, 2, [255, 0, 0]);
+    expect(newCell.positionX).toBe(1);
+    expect(newCell.positionX).toBe(2);
+    expect(newCell.color).toBe([255, 0, 0]);
+  });
 });
