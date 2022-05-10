c36295312c83d440d3c79e5b6895274e3110d0dc
Julien <jujufrancois1999@gmail.com>
2022-05-10 10:44:58 +0200

    Fixed safari issu

--- src/core/index.spec.ts
+++ src/core/index.spec.ts
@@ -2,11 +2,12 @@
 import * as matchers from 'jest-extended';
 expect.extend(matchers);
 import { Cell } from '../core';
+import { Generation } from '../core';
 
 describe('Test constructor of class Cell.', function () {
   it('Should pass', function () {
     const newCell = new Cell(1, 2, [255, 0, 0]);
-    expect(newCell.positionX).toBe(1);
+    expect(Generation.countNeighbourLivingCells()).toBe(1);
     expect(newCell.positionX).toBe(2);
     expect(newCell.color).toBe([255, 0, 0]);
   });
